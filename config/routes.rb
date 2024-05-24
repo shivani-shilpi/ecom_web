be2889f58ca59afb312a1dc6bc5d111a1b4cd42a
MMChavda <mohitmchavda@gmail.com>
2024-05-24 13:31:00 +0530

    New project

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "home#index"
+
+  authenticated :admin_user do 
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
+end
